13837238eec15de7e5ba3d558266e89bf648119c
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 19:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let answer = confirm("Do you want to proceed?");
+
+if (answer) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
